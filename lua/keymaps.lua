05f4a50c2fa2dd87f8bee1527e924fb883f46980
Ryuhei18 <ryuhei.otani@interfirm.co.jp>
2025-01-31 19:17:16 +0900

    feat: keymaps.luaの編集

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,12 +1,13 @@
-local opts = { nnoremap = true, silent = true }
+local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
 
 --local keymap = vim.keymap
 local keymap = vim.api.nvim_set_keymap
 
--- Remap space as leader key
+--Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Modes
 --   normal_mode = 'n',
@@ -45,39 +46,71 @@ keymap("n", "x", '"_x', opts)
 keymap("n", "dw", 'vb"_d', opts)
 
 keymap("n", "diw", "ciw", opts)
+
+keymap("n", "fff", "<cmd>Telescope find_files hidden=true<CR>", opts)
+keymap("n", "ffg", "<cmd>Telescope live_grep<CR>", opts)
+keymap("n", "ffb", "<cmd>Telescope buffers<CR>", opts)
+keymap("n", "ffh", "<cmd>Telescope help_tags<CR>", opts)
+
+keymap("n", "<leader>n :NvimTreeFocus<CR>", opts)
+keymap("n", "<C-t>", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<C-n>", ":NvimTreeOpen<CR>", opts)
+keymap("n", "<C-f>", ":NvimTreeFindFile<CR>", opts)
+
+keymap("n", "<silent> tt", "<cmd>term<CR><C-\\><C-N>", opts)
+keymap("n", "<silent> tt", "<cmd>belowright new<CR><cmd>term<CR><C-\\><C-N>", opts)
+keymap("n", "<silent> te", "<cmd>tabnew<CR>", opts)
+
+keymap("n", "sp", "<cmd>split<CR><C-w>w", opts)
+keymap("n", "vs", "<cmd>vsplit<CR><C-w>w", opts)
+
+keymap("n", "<C-w><C-w>", "gt", opts)
+keymap("n", "<C-w>e", "gT", opts)
+
+keymap("n", "diw", "ciw", opts)
 keymap("n", "ciw", "diw", opts)
 keymap("n", "xiw", "ciw", opts)
+
+keymap("n", "dd", "d^d$", opts)
+keymap("n", "cc", "dd", opts)
 
 keymap("n", "x<up>", "ddkP", opts)
 keymap("n", "x<down>", "ddp", opts)
 
--- ;でコマンド入力( ;と:を入れ替)
-keymap("n", ";", ":", opts)
-
--- 行末までのヤンクにする
 keymap("n", "Y", "y$", opts)
 
--- <Space>q で強制終了
-keymap("n", "<Space>q", ":<C-u>q!<Return>", opts)
+keymap("t", "<Esc>", "<C-\\><C-N>", opts)
+keymap("t", "fff", "<C-w><C-n><C-w><C-w><C-\\><C-N>:Telescope find_files hidden=true<CR>", opts)
+keymap("t", "ffg", "<C-\\><C-N>:Telescope live_grep<CR>", opts)
+keymap("t", "ffb", "<C-\\><C-N>:Telescope buffers<CR>", opts)
 
--- ESC*2 でハイライトをやめる
-keymap("n", "<Esc><Esc>", ":<C-u>set nohlsearch<Return>", opts) 
+keymap("n", "<Tab><Tab>", ">>", opts)
+keymap("n", "<Tab><S-Tab>", "<<", opts)
 
--- Insert --
--- Press jk fast to exit insert mode
+keymap("v", "<Tab><Tab>", ">>gv", opts)
+keymap("v", "<Tab><S-Tab>", "<<gv", opts)
 
-keymap("i", "jk", "<Esc>", opts)
-
--- コンマの後に自動的にスペースを挿入
-keymap("i", ",", ",<Space>", opts)
-
--- Visual --
--- Stay in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
+
+keymap("n", "<S-left>", "v<left>", opts)
+keymap("n", "<S-right>", "v<right>", opts)
+keymap("n", "<S-up>", "v<up>", opts)
+keymap("n", "<S-down>", "v<down>", opts)
+
+keymap("v", "<S-left>", "<left>", opts)
+keymap("v", "<S-right>", "<right>", opts)
+keymap("v", "<S-up>", "<up>", opts)
+keymap("v", "<S-down>", "<down>", opts)
+
+keymap("n", "<C-left>", "<C-w>h", opts)
+keymap("n", "<C-right>", "<C-w>l", opts)
+keymap("n", "<C-up>", "<C-w>k", opts)
+keymap("n", "<C-down>", "<C-w>j", opts)
+
+keymap("n", "c<up>", "dd", opts)
+keymap("n", "c<down>", "dd", opts)
 
 -- ビジュアルモード時vで行末まで選択
 keymap("v", "v", "$h", opts)
 
--- 0番レジスタを使いやすくした
-keymap("v", "<C-p>", '"0p', opts)
