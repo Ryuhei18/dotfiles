cf89e9f79943df4f7f3db8e35f0083c5394857bb
Ryuhei18 <ryuhei.otani@interfirm.co.jp>
2025-02-06 12:20:48 +0900

    feat: plugin/*ディレクトリ内のファイル追加

---
+++ plugin/neosolarized.lua
@@ -0,0 +1,37 @@
+local ok_status, NeoSolarized = pcall(require, "NeoSolarized")
+
+if not ok_status then
+  return
+end
+
+-- Default Setting for NeoSolarized
+
+NeoSolarized.setup {
+  style = "light",
+  transparent = false,
+  terminal_colors = true,
+  enable_italics = true,
+  styles = {
+    -- Style to be applied
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = { bold = true },
+    variables = {},
+    string = { italic = true },
+    underline = true, -- true/false
+    undercurl = true, -- true/false
+  },
+  -- Add specific highlights
+  on_highlights = function(highlights, colors)
+    -- highlights.Include.fg = colors.red --Using `red` foregroup for Includes
+  end,
+}
+-- Set colorscheme to NeoSolarized
+vim.cmd([[
+  try
+    colorscheme NeoSolarized
+  catch /^Vim\%((\a\+)\)\=:E18o
+    colorscheme default
+    set background=dark
+  endtry
+]])
